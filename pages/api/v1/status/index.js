2987c10b5fc82f2b058db0d243edc0fb2382a81b
Rafael <rafaelramos0401@example.com>
2024-05-03 11:26:50 -0300

    add my solution to day 20 challenge

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,18 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersion = await database.query("SHOW SERVER_VERSION;")
+  const postgresMaxConnection = await database.query("SHOW max_connections;")
+  const postgresConnections = await database.query("SELECT * FROM pg_stat_activity WHERE state = 'active';")
+  const activeClientsNumber = postgresConnections.rowCount
+
   response.status(200).json({
-    chave: "São acima da média"
+    updated_at: updatedAt,
+    postgres_version: parseInt(postgresVersion.rows[0].server_version),
+    postgres_maximum_connections: parseInt(postgresMaxConnection.rows[0].max_connections),
+    postgres_active_connections: activeClientsNumber
   })
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json()
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.postgres_version).toBeDefined();
+  expect(responseBody.postgres_maximum_connections).toBeDefined();
+  expect(responseBody.postgres_active_connections).toBeDefined();
+
+  console.log(responseBody)
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+  expect(responseBody.postgres_version).not.toBeNaN()
+  expect(responseBody.postgres_maximum_connections).not.toBeNaN()
+  expect(responseBody.postgres_active_connections).not.toBeNaN()
 })
